cd7a1a9757b90a69fd50d26e0021518fb529a860
Diego Prieto Torres <diego.alfonso.prieto.torres@gmail.com>
2023-12-19 16:52:41 -0500

    feat: run sh with params

--- src/scripts/execute_sql_files.sh
+++ src/scripts/execute_sql_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Specify the folder path
-folder_path="./src/1_structure"
+folder_path = $0
+echo "Processing folder: $folder_path"
 
 for file in "$folder_path"/*; do
     if [ -f "$file" ]; then
         echo "Processing file: $file"
+        psql -h localhost -U your_username -d your_database -a -f $file 
     fi
 done
